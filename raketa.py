76c0a92daa60af2fa2972528eec81de706297c4b
DanilPankratov1108 <90336955+DanilPankratov1108@users.noreply.github.com>
2021-10-31 23:31:27 +0300

    Update raketa.py

--- raketa.py
+++ raketa.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from matplotlib import pyplot as pp
 
 MODEL_G = 9.81
@@ -11,7 +12,7 @@ class Body:
         self.y = y
         self.vx = vx
         self.vy = vy
-
+        
         self.trajectory_x = []
         self.trajectory_y = []
 
@@ -20,38 +21,42 @@ class Body:
         self.trajectory_x.append(self.x)
         self.trajectory_y.append(self.y)
 
-
         self.x += self.vx * MODEL_DT
-        self.y += self.vy * MODEL_DT
+        self.y += self.vy * MODEL_DT - (MODEL_G * MODEL_DT**2) / 2
         self.vy -= MODEL_G * MODEL_DT
-
-
+       
+        
 class Rocket(Body):
 
-    def __init__(self, x, y, mr, mo, ux, uy, dm):
+    def __init__(self, x, y, wx, wy, ux, uy, mr, mo, dm):
         
-        super().__init__(x, y, 10, 100)
-
+        super().__init__(0, 0, 10, 100)
+        
         self.x = x
         self.y = y
         self.mr = mr
         self.mo = mo
+        self.wx = wx
+        self.wy = wy
         self.ux = ux
         self.uy = uy
         self.dm = dm
         self.M = mr + mo
 
-
     def advance(self):
         super().advance()
-        self.mo += self.dm * MODEL_DT
-        self.M += self.dm * MODEL_DT
-        self.vx +=- (self.ux * self.dm) / self.M * MODEL_DT
-        self.vy +=- (self.uy * self.dm) / self.M * MODEL_DT
-        
-            
+        self.wx = self.vx - self.ux
+        self.wy = self.vy - self.uy
+        if self.dm < 0 and self.mo > 0:
+            self.M += self.dm * MODEL_DT
+            self.mo += self.dm * MODEL_DT    
+            if MODEL_DT * MODEL_G <= -((self.wy * self.dm) / (self.M)) * MODEL_DT:
+                    self.vx += -((self.dm * self.wx) / self.M) * MODEL_DT
+                    self.vy += -((self.dm * self.wy) / self.M) * MODEL_DT
+            else:
+                    self.vy -= MODEL_DT * MODEL_G                       
 b = Body(0, 0, 10, 100)
-r = Rocket(0, 0, 1, 15, 1, 2, -3) 
+r = Rocket(0, 0, 10, 10, 5, 5, 14, 15, -3) 
 
 bodies = [b, r]
 
