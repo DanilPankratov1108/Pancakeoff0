11f12bd99bf3eed51454ac2ab02dae5a364d3d18
DanilPankratov1108 <90336955+DanilPankratov1108@users.noreply.github.com>
2021-11-26 23:34:45 +0300

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -5,19 +5,21 @@ def test_function(n):
 
 def repeat(n):
     def dec(genuine_function):
-        def fake_function(*args, **kwargs):
-            result = genuine_function(*args, **kwargs)
+        def fake_function(arg):
+            result = arg
+            for i in range(n):
+                result = genuine_function(result)
             return result
         return fake_function
     return dec
 
 @repeat(2)
-def plus_1(y, x):
-    return y + x + 1
+def plus_1(x):
+    return x + 1
 
 @repeat(0)
-def mul_2(y, x):
-    return y + x * 2
+def mul_2(x):
+    return x * 2
 
-print(plus_1(2,2))
-print(mul_2(0,2))
+print(plus_1(3))
+print(mul_2(4))
