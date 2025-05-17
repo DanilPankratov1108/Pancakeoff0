184bf8b81301ebd01e1a9d16d3f80eaec62c16d9
DanilPankratov1108 <90336955+DanilPankratov1108@users.noreply.github.com>
2025-05-17 12:44:59 +0300

    Update runze6.py

--- mypump/runze6.py
+++ mypump/runze6.py
@@ -24,23 +24,21 @@ parity = PARITY_NONE
 
 """Подключение и выбор порта"""
 
-ports = list(serial.tools.list_ports.comports())
 target_manufacturer = 'wch.cn'
-target_pid = 29987
-target_vid = 6790
-com_port = None
-print("Доступные ком порты:")
-for port in ports:
-    print(port.device)
+target_hwid = 'USB VID:PID=1A86:7523 SER= LOCATION=1-2'
 
-if port.pid == target_pid:
-    com_port = port.device
-    print(f'Подключение к {com_port}')
-else:
-    raise ValueError('No required Com port found')
+def find_port(target_vid, target_pid):
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        device_info = port.hwid
+        if f"{target_vid}" in device_info and f"{target_pid}" in device_info:
+            return port.device
+    return None
 
-if port is None:
-    raise ValueError('No COM port found')
+target_pid = "1A86"
+target_vid = "7523"
+
+com_port = find_port(target_vid, target_pid)
 
 try:
             ser = serial.Serial(com_port,
@@ -69,8 +67,6 @@ except KeyboardInterrupt:
 PUMP = 'SY-01B'     # модель насоса Runze Fluid
 HEAD = 'M12'        # головка в насосе
 VALVE = 12          # число клапанов в головке
-VOLUME = 125        # объём шприца в мкл
-SQUARE = 2.083      # площадь сечения шприца в мм^2
 
 """Важные константы"""
 
@@ -85,17 +81,12 @@ DEFAULT_VEL = 4000  # скорость по умолчанию
 
 """2)В абсолютных единицах(мкл и мкл/мин)"""
 
-REAL_NOM_STEP = 30          # номинальный ход
+REAL_NOM_STEP = 62.5          # номинальный ход
 REAL_MAX_STEP = 125         # максимальный ход
-REAL_MIN_STEP = 1/200       # минимальный ход
-REAL_MAX_VEL = 960          # максимальная скорость
-REAL_MIN_VEL = 6.25         # минимальная скорость
-REAL_DEFAULT_VEL = 640      # скорость по умолчанию
-
-"""Коэффициенты перевода"""
-
-k1 = (1.25 * 10 ** -7) / (1.3 * 10 ** -9)       # Перевод объёма из мкл в шаги
-k2 = 6.25                                       # Перевод скорости из мкл/мин в шаги/сек
+REAL_MIN_STEP = 1/96       # минимальный ход
+REAL_MAX_VEL = 3750          # максимальная скорость
+REAL_MIN_VEL = 1.6         # минимальная скорость
+REAL_DEFAULT_VEL = 2500      # скорость по умолчанию
 
 """Битовые строки, которые идут от насоса"""
 
@@ -106,36 +97,20 @@ state_free = b'\xff/0`\x03\r\n'     #Свободен
 
 init_error = b'\xff/0a\x03\r\n'     #Ошибка инициализации
 invalid_param = b'\xff/0c\x03\r\n'   #Недопустимые параметры
-EEPROM_failure = b'\xff/0f\x03\r\n'     #Проблемы с EEPROM
+# EEPROM_failure = b'\xff/0f\x03\r\n'     #Проблемы с EEPROM
 not_init = b'\xff/0g\x03\r\n'   #Попытка начать работу без инициализации
 internal_failure = [b'\xff/0h\x03\r\n', b'\xff/0l\x03\r\n']  #Внутренний сбой
 plunger_overload = b'\xff/0i\x03\r\n'   #Перегрузка поршня
 valve_overload = b'\xff/0j\x03\r\n'     #Перегрузка клапана
 plunger_move = b'\xff/0k\x03\r\n'   #Движение поршня ограниченно
 AD_failure = b'\xff/0n\x03\r\n'     #Неисправность аналого-цифрового преобразователя
-command_overflow = b'\xff/0O\x03\r\n'   #Переполнена очередь команд
+# command_overflow = b'\xff/0O\x03\r\n'   #Переполнена очередь команд
 
 """Некоторые команды в ASCII"""
 
 state = '/1QR' + '\r\n' #для проверки состояния
 stop = '/1TR' + '\r\n'  #для экстренной остановки
 report = '/1?R' + '\r\n'    #запрашивает текущий объём
-
-# def checks():
-#     return check
-
-# """Время работы кода"""
-#
-# def time_work(T: long):
-#     com = list()
-#     global running
-#     while
-#         running = True
-
-# """Флаг для запуска работы на время"""
-
-running = False
-terminate = False
 
 """Декоратор, который навешиваетя на некоторые функции в библиотеке. Он позволяет определять состояние выполнения функций насоса."""
 
@@ -144,11 +119,20 @@ def check_state_pump(func):
                 stop_thread = threading.Thread(target=args[0].stop_device, daemon = True)
                 stop_thread.start()
                 ser.write(str.encode(state, encoding='ascii'))
-                N = ser.read_until(expected = state_work)
-                while N == state_work and not terminate:
+                N = ser.read(7)
+                while N == state_work and not mypump.terminate:
                     ser.write(str.encode(state, encoding='ascii'))
                     N = ser.read_until(expected = state_work)
-                if terminate:
+                if N == plunger_overload or N == valve_overload:
+                    logging.error('Overload of plunger or valve')
+                    raise Warning('Перегрузка поршня или клапанов. Рекомендуется сделать инициализацию')
+                elif N in internal_failure:
+                    logging.error('Internal failure of device.')
+                    raise Warning('Внутренний сбой устройства. Рекомендуется обратиться к производителям')
+                elif N == AD_failure:
+                    logging.error('A/D converter failure.')
+                    raise Warning('Неисправность A/D-преобразователя.')
+                if mypump.terminate:
                     raise Warning('Остановка. Нажата клавиша t')
                 return func(*args, **kwargs)
     return wrapper
@@ -157,11 +141,7 @@ def check_state_pump(func):
 
 class mypump:
 
-    def __init__(self):
-        self.volume1 = 0
-        self.R = R
-        # self.Volume1 = 0
-        # self.N2 = N2
+    terminate = False   #Флаг остановки насоса
 
     """Опрос состояния выполнения команд в насосе: Free - свободен, Busy - занят"""
 
@@ -186,10 +166,29 @@ class mypump:
             logging.info('Stopping with /1TR command. It is recommended to do initialization.')
             terminate = True
 
+    # def minitimer(self, func, *args, duration):
+    #     self.running = True
+    #     d = duration*3600
+    #     end_time = time.time() + d
+    #     def run():
+    #         while self.running and time.time() < end_time:
+    #             func(*args)
+    #             time.sleep(1)
+    #     command_thread = threading.Thread(target = run, daemon = True)
+    #     command_thread.start()
+    #     time_thread = threading.Timer(duration, self.stop)
+    #     time_thread.start()
+    #
+    # def timer(self, commands, duration):
+    #     for command in commands:
+    #         func = command
+    #         self.minitimer(func, duration = duration)
+
     """Это остановка командой stop"""
 
-    def stop():
+    def stop(self):
         ser.write(str.encode(stop, encoding='ascii'))
+        self.running = False
         print('Остановка. Рекомендуется сделать реинициализацию')
         logging.info('Stopping with /1TR command. It is recommended to do initialization.')
 
@@ -201,49 +200,38 @@ class mypump:
         print(f'Пауза {T} мин')
         time.sleep(t)
 
+    """Установка максимального объёма"""
+    def set_volume(self, V):
+        if V == 125 or V == 500:
+            k1 = MAX_STEP / V  # Перевод объёма из мкл в шаги
+            k2 = k1 / 60    # Перевод скорости из мкл/мин в шаги/сек
+            self.k1 = k1
+            self.k2 = k2
+            print(f'Вы установили объём шприца {V} мкл')
+            logging.info(f'Max syringe volume set: {V} ul')
+        else:
+            raise ValueError('Шприцов с таким объёмом не существует')
+
     """Инициализация"""
 
     @check_state_pump
     def init(self):
             init = str('/1ZR') + '\r\n'
             ser.write(str.encode(init, encoding = 'ascii'))
-            print('Инициализация')
-            logging.info('Initialization.')
-            I = ser.read_until(expected = init_error)
+            I = ser.read(7)
             if I == init_error:
                 raise Exception('Ошибка инициализации. Попробуйте повторить попытку')
+            else:
+                print('Инициализация')
+                logging.info('Initialization.')
 
-    """Ввод объёма в мкл с заданной скоростью в мкл/мин из определённого клапана"""
 
-    @check_state_pump
-    def refill(self, volume1, speed: float, n1: int):
-        self.volume1 = volume1
-        v1 = int(volume1 * k1)
-        sp1 = int(speed * k2)
-        command1 = f'/1I{n1}V{sp1}IA{v1}R' + '\r\n'
-        ser.write(str.encode(command1, encoding='ascii'))
-        I = ser.read(7)
-        if I == not_init:
-            raise Warning('Попытка начать работу без инициализации.')
-        elif I == invalid_param:
-            logging.error(
-                 f'Invalid values specified:{volume1} ul, {speed} ul/min, {n1} valve. Read info about acceptable values.\n')
-            raise ValueError('Указаны недопустимые параметры, читайте инструкцию.')
-        else:
-                ser.write(str.encode(report, encoding='ascii'))
-                R = ser.read(12)
-                print(f'Введён объём {volume1} мкл в шприц из {n1}-го клапана\n'
-                f'Скорость: {speed} мкл/мин\n'
-                )
-                logging.info(f'Volume input: {volume1} ul, valve: {n1}, velocity: {speed} ul/min.')
-
-    """Добавление объёма к уже установленному из выбранного клапана с заданной скоростью. Используется, если до этого набирался какой-либо объём командой set_volume()"""
+    """Всасывание объёма в мкл в шприц с заданной скоростью в мкл/мин из определённого клапана"""
 
     @check_state_pump
-    def add(self, Volume1, speed: float, n2: int):
-        # self.Volume1 = Volume1
-        v2 = int(Volume1 * k1)
-        sp2 = int(speed * k2)
+    def refill(self, Volume1, speed: float, n2: int):
+        v2 = int(Volume1 * self.k1)
+        sp2 = int(speed * self.k2)
         command2 = f'/1V{sp2}I{n2}P{v2}R' + '\r\n'
         ser.write(str.encode(command2, encoding='ascii'))
         I = ser.read(7)
@@ -255,23 +243,18 @@ class mypump:
             raise ValueError('Указаны недопустимые параметры, читайте инструкцию.')
         elif I == plunger_move:
             raise ValueError('Движение поршня невозможно из-за ограничений по объёму.')
-        else:
-            ser.write(str.encode(report, encoding = 'ascii'))
-            R1 = ser.read(11)
-            self.volume1 += Volume1
-            print(f'Добавлен объём {Volume1} мкл в шприц с текущим объёмом {self.volume1-Volume1} мкл из {n2}-го клапана\n'
-                  f'Скорость: {speed} мкл/мин\n'
-                  f'Конечный объём: {self.volume1} мкл')
-            logging.info(f'Volume add: {Volume1} ul, valve: {n2}, velocity: {speed} ul/min. Summary volume: {self.volume1}')
+        print(f'Добавлен объём {Volume1} мкл в шприц из {n2}-го клапана\n'
+              f'Скорость: {speed} мкл/мин\n')
+        logging.info(f'The volume {Volume1} ul was added, valve: {n2}, velocity: {speed} ul/min.')
+        self.report_volume()
 
     """Вывод объёма в мкл c заданной скоростью в выбранный клапан"""
 
     @check_state_pump
     def infuse(self, volume2, speed: float, n3: int):
-        x = self.volume1 - volume2
-        v3 = int(x * k1)
-        sp3 = int(speed * k2)
-        command3 = f'/1V{sp3}O{n3}A{v3}R' + '\r\n'
+        v3 = int(volume2 * self.k1)
+        sp3 = int(speed * self.k2)
+        command3 = f'/1V{sp3}O{n3}D{v3}R' + '\r\n'
         ser.write(str.encode(command3, encoding='ascii'))
         I = ser.read(7)
         if I == not_init:
@@ -279,16 +262,42 @@ class mypump:
         elif I == invalid_param:
             logging.error(f'Invalid value(-s) specified:{volume2} ul, {speed} ul/min, {n3} valve')
             raise ValueError('Указаны недопустимые параметры. читайте инструкцию.')
+        elif I == plunger_move:
+            raise ValueError('Движение поршня невозможно из-за ограничений по объёму.')
+        print(f'Выводится объём {volume2} мкл в {n3}-й клапан\n'
+              f'Скорость: {speed} мкл/мин\n'
+              )
+        logging.info(f'The {volume2} ul was infused, valve: {n3}, velocity: {speed} ul/min.')
+        self.report_volume()
+
+    """Функция для вывода текущего объёма в шприце"""
+
+    @check_state_pump
+    def report_volume(self):
+        ser.write(str.encode(report, encoding='ascii'))
+        R = ser.read(12)
+        start_index = R.index(b'`') + 1
+        end_index = R.index(b'\x03')
+        number_bytes = R[start_index:end_index]
+        number_str = number_bytes.decode('utf-8')
+        number = int(number_str)
+        final_volume = number / self.k1
+        print(f"Текущий объём в шприце: {final_volume} мкл\n")
+        logging.info(f'Report command was send. There is {final_volume} ul in the syringe.')
+        ser.send_break(2)
+
+    """Функция для смены шприца"""
+
+    @check_state_pump
+    def change_syringe(self):
+        change = f'/1V4000A12000R' + '\r\n'
+        ser.write(str.encode(change, encoding = 'ascii'))
+        r = ser.read(7)
+        if r == not_init:
+            raise Warning('Попытка начать работу без инициализации.')
         else:
-            print(f'Выводится объём {volume2} мкл в {n3}-й клапан\n'
-                  f'Скорость: {speed} мкл/мин\n'
-                  )
-            logging.info(f'Volume output: {volume2} ul, valve: {n3}, velocity: {speed} ul/min.')
-            self.volume1 = volume2
+            print('Замените шприц')
+            logging.info('Сommand was entered to replace syringe.')
 
 if __name__ == "__main__":
-    mp = mypump()
-
-# """Объект класса"""
-#
-# mp = mypump()
+    mypump = mypump()
